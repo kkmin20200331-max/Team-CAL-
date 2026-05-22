f6a1f41412e04f11a2480323cd798a9ba07ed370
kyungmin <k76054266@gmail.com>
2026-05-22 18:16:55 +0900

    feat: 회원가입 페이지 UI 및 2단계 스텝 기능 구현

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -2,8 +2,11 @@ import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router';
 import { ThemeProvider } from 'next-themes';
 import { Toaster } from 'sonner';
 
+// Auth pages
+import Login from './pages/auth/Login';
+import SignUp from './pages/auth/Signup';
+
 // Admin pages
-import AdminLogin from './pages/admin/AdminLogin';
 import BranchSelection from './pages/admin/BranchSelection';
 import AdminDashboard from './pages/admin/AdminDashboard';
 import WeeklySchedule from './pages/admin/WeeklySchedule';
@@ -17,7 +20,6 @@ import CustomerAnalytics from './pages/admin/CustomerAnalytics';
 import MultibranchDashboard from './pages/admin/MultibranchDashboard';
 
 // Employee pages
-import EmployeeLogin from './pages/employee/EmployeeLogin';
 import EmployeeHome from './pages/employee/EmployeeHome';
 import MySchedule from './pages/employee/MySchedule';
 import QRCheckIn from './pages/employee/QRCheckIn';
@@ -31,11 +33,15 @@ export default function App() {
     <ThemeProvider attribute="class" defaultTheme="light">
       <Router>
         <Routes>
-          {/* Root redirect */}
-          <Route path="/" element={<Navigate to="/admin" replace />} />
+          {/*{Auth routes}*/}
+            {/* 첫 화면(/)으로 접속 시 새로 만든 통합 로그인 화면으로 자동 리다이렉트 */}
+            <Route path="/" element={<Navigate to="/auth/login" replace />} />
+            {/* /auth 로 들어와도 자동으로 /auth/login 으로 안전하게 이동 */}
+            <Route path="/auth" element={<Navigate to="/auth/login" replace />} />
+            <Route path="/auth/login" element={<Login />} />
+            <Route path="/auth/signup" element={<SignUp />} />
 
           {/* Admin routes */}
-          <Route path="/admin" element={<AdminLogin />} />
           <Route path="/admin/branch-selection" element={<BranchSelection />} />
           <Route path="/admin/dashboard/:branchId" element={<AdminDashboard />} />
           <Route path="/admin/multibranch" element={<MultibranchDashboard />} />
@@ -49,7 +55,6 @@ export default function App() {
           <Route path="/admin/analytics/:branchId" element={<CustomerAnalytics />} />
 
           {/* Employee routes */}
-          <Route path="/employee" element={<EmployeeLogin />} />
           <Route path="/employee/home" element={<EmployeeHome />} />
           <Route path="/employee/schedule" element={<MySchedule />} />
           <Route path="/employee/checkin" element={<QRCheckIn />} />
